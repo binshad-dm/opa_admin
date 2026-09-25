308e17dbfa5170d1b365ee80c65bc383b67d05ed
binshad-dm <binshad.p@mediwarehms.com>
2026-09-25 12:16:32 +0530

    added semantics code in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,13 +11,13 @@ import 'core/service_locator.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initServiceLocator();
-
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+    SemanticsBinding.instance.ensureSemantics();
+  });
   runApp(
     BlocProvider<LocaleCubit>(
       create: (context) => LocaleCubit(sl<SharedPreferences>()),
-      child: const AppRoot(
-        initialRoute: AppRoutes.authorizationDashboard,
-      ),
+      child: const AppRoot(initialRoute: AppRoutes.authorizationDashboard),
     ),
   );
 }
